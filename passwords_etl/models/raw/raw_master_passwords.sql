be04f30e7aea4daee6016cf28c5c5329486b7f90
Danil <git@brcloud.io>
2025-11-28 21:42:43 +0200

    Fix: changed_at timestamp

--- passwords_etl/models/raw/raw_master_passwords.sql
+++ passwords_etl/models/raw/raw_master_passwords.sql
@@ -7,8 +7,4 @@ with raw_master_passwords as
          (select *, filename
           from {{source('raw', 'mp')}})
 select hash, filename
-{#       strptime(#}
-{#               regexp_extract(filename, '([0-9]{14})'),#}
-{#               '%Y%m%d%H%M%S'#}
-{#       ) as export_ts#}
 from raw_master_passwords
